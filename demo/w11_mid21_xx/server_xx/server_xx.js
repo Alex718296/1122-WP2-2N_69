99b92df95b1b3a3ee4b1f47bd9f32e912cda067c
Alex718296 <as71829696@gmail.com>
2024-05-01 22:44:11 +0800

    W11-P3: Use Postman to test DELETE /api/blogs/clearAll, and POST /api/blogs/loadAll

--- demo/w11_mid21_xx/server_xx/server_xx.js
+++ demo/w11_mid21_xx/server_xx/server_xx.js
@@ -29,9 +29,19 @@ app.get('/api/blogs', (req, res) => {
   res.json({ blogList_xx });
 });
 
-app.post('/api/blogs', (req, res) => {});
+app.post('/api/blogs/loadAll', (req, res) => {
+  blogList_xx = blogs_xx;
+  console.log('blogLisrt_xx', JSON.stringify(blogList_xx));
+  res.json({ msg: 'load all blogs' });
+});
 
 app.patch('/api/blogs/:id', (req, res) => {});
+
+app.delete('/api/blogs/cleanAll', (req, res) => {
+  blogList_xx = [];
+  console.log('blogList_xx', JSON.stringify(blogList_xx));
+  res.json({ msg: 'clear all blogs' });
+});
 
 app.delete('/api/blogs/:id', (req, res) => {
   const { id } = req.params;
